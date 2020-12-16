0b4184bf1bf68d188c0cf830a4840c10e8adacd5
KorwinBieniek <korwin-bieniek@wp.pl>
2020-12-16 14:13:17 +0100

    Next step on MP3 Player project. Added .fxml file, added a label to the project.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module javafx {
     requires javafx.graphics;
-    exports pl.javastart.hellofx to javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
+
     exports pl.javastart.mp3player.main to javafx.graphics;
+    exports pl.javastart.hellofx to javafx.graphics;
 }
