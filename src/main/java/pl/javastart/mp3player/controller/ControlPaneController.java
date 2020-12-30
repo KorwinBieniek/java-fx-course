5e0cfa689943a9d62155e4f4ec18d095281a23f8
KorwinBieniek <korwin-bieniek@wp.pl>
2020-12-30 22:19:38 +0100

    MP3 Player Project - Configured volume slider and song progress slider. Bound properties to them and added numerical values to show up in the console displaying the current volume.

--- src/main/java/pl/javastart/mp3player/controller/ControlPaneController.java
+++ src/main/java/pl/javastart/mp3player/controller/ControlPaneController.java
@@ -26,11 +26,22 @@ public class ControlPaneController {
         System.out.println("Control controller created");
         configureButtons();
         configureVolume();
+        configureSliders();
     }
 
     private void configureVolume() {
         volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->
                 System.out.println("Wciśnięto przycisk na suwaku głośności")
+        );
+    }
+
+    private void configureSliders() {
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                System.out.println("Zmiana głośności " + newValue.doubleValue())
+        );
+
+        progressSlider.valueProperty().addListener(x ->
+                System.out.println("Przesunięcie piosenki")
         );
     }
 
