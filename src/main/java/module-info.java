d0fe0883e02d860bafa9aafed84e6d39e7f4aa3b
KorwinBieniek <korwin-bieniek@wp.pl>
2021-01-30 19:24:55 +0100

    MP3 Player Project - added jid3lib library to read the attributes from mp3 file. Added test mp3 file to display in the application.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module javafx {
+module mp3player {
     requires javafx.graphics;
-    requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.fxml;
+    requires jid3lib;
 
     exports pl.javastart.mp3player.main to javafx.graphics;
-    exports pl.javastart.hellofx.main to javafx.graphics;
-    opens pl.javastart.hellofx.controller to javafx.fxml;
     opens pl.javastart.mp3player.controller to javafx.fxml;
+    opens pl.javastart.mp3player.mp3 to javafx.base;
 }
